b2b24b6db3d0f8f28c42164f40d33fff3b7d65fd
Çetin Özdil <cetinozdil@indas.com.tr>
2022-05-18 18:27:15 +0300

    more refactorings

--- WebHost/Startup.cs
+++ WebHost/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,62 +19,48 @@ namespace WebHoster
         private readonly IConfiguration config;
         private readonly IStartupInjectionConfiguration injectionConfig;
 
-        public Startup(IWebHostEnvironment env, IConfiguration configuration, IStartupInjectionConfiguration siConfig)
+        public Startup(IWebHostEnvironment env, IConfiguration configuration, IStartupInjectionConfiguration startupInjectionConfig)
         {
             HostingEnvironment = env;
 
             config = configuration;
-            injectionConfig = siConfig;
-        }
-
-        public IWebHostEnvironment HostingEnvironment { get; }
-
-        public void Configure(IApplicationBuilder app)
-        {
-            if (HostingEnvironment.IsDevelopment())
-                app.UseDeveloperExceptionPage();
-
-            // if using ssl use redirect too
-            if(config.GetValue<bool>("UseSSL"))
-                app.UseHttpsRedirection();
-
-            // for testing allow all cors requests
-            if (config.GetValue<bool>("RelaxedCors"))
-            {
-                app.UseCors(x => x
-                    .AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader());
-            }
-
-            app.UseRouting();
-
-            if (injectionConfig.AuthInjection != null)
-                injectionConfig.AuthInjection.InjectConfig(app);
-
-            foreach (var item in injectionConfig.ApplicationInjection)
-                item.InjectConfig(app);
-
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            injectionConfig = startupInjectionConfig;
         }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            // if using ssl use redirect too
-            if (config.GetValue<bool>("RelaxedCors"))
+            // relaxed cors
+            if (config.GetValue<bool>("UseRelaxedCors"))
                 services.AddCors();
+
+            // ssl redirect
+            if (config.GetValue<bool>("UseSSL"))
+            {
+                var sslPort = config.GetValue<int>("SSLport");
+
+                services.AddHttpsRedirection(options =>
+                {
+                    options.HttpsPort = sslPort;
+                    options.RedirectStatusCode = StatusCodes.Status301MovedPermanently;
+                });
+            }
 
             services.AddResponseCaching(options =>
             {
                 options.SizeLimit = 250;
             });
 
-            services.AddResponseCompression(options =>
+            // use compression
+            if (config.GetValue<bool>("UseCompression"))
             {
-                options.Providers.Add<GzipCompressionProvider>();
-                options.Providers.Add<BrotliCompressionProvider>();
-            });
+                services.AddResponseCompression(options =>
+                {
+                    options.Providers.Add<GzipCompressionProvider>();
+                    options.Providers.Add<BrotliCompressionProvider>();
+                });
+            }
 
+            // json config
             services.AddControllers().AddNewtonsoftJson((options) =>
             {
                 options.UseMemberCasing();
@@ -84,11 +71,47 @@ namespace WebHoster
                 options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
             });
 
+            // auth injections
             if (injectionConfig.AuthInjection != null)
                 injectionConfig.AuthInjection.InjectConfigureServices(services);
 
+            // app injections
             foreach (var item in injectionConfig.ApplicationInjection)
                 item.InjectConfigureServices(services);
         }
+
+        public IWebHostEnvironment HostingEnvironment { get; }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (HostingEnvironment.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            // if using ssl use redirect too
+            if (config.GetValue<bool>("UseSSL"))
+                app.UseHttpsRedirection();
+
+            // for testing allow all cors requests
+            if (config.GetValue<bool>("UseRelaxedCors"))
+            {
+                app.UseCors(x => x
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            }
+
+            app.UseRouting();
+
+            // auth injections
+            if (injectionConfig.AuthInjection != null)
+                injectionConfig.AuthInjection.InjectConfig(app);
+
+            // app injections
+            foreach (var item in injectionConfig.ApplicationInjection)
+                item.InjectConfig(app);
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+
     }
 }
--- WebHost/WebBuilder.cs
+++ WebHost/WebBuilder.cs
@@ -13,15 +13,19 @@ namespace WebHoster
 {
     public class WebBuilder
     {
-        private string _webRoot = string.Empty;
-        private bool _useSSL = false;
-        private int _sslPort = 5001;
-        private int _httpPort = 5000;
-        private string _certificatePath = string.Empty;
-        private string _certificatePass = string.Empty;
-        private IPAddress _listenIP = IPAddress.Any;
+        private string webRoot = string.Empty;
+
+        private IPAddress listenIP = IPAddress.Any;
+        private int httpPort = 5000;
+
+        private bool useSSL = false;
+        private int sslPort = 5001;
+        private string sslCertificatePath = string.Empty;
+        private string sslCertificatePass = string.Empty;
 
         private bool relaxedCorsPolicy = false;
+        private bool useCompression = false;
+
 
         private readonly StartupInjectionConfiguration _startupConfiguration = new StartupInjectionConfiguration();
 
@@ -45,69 +49,77 @@ namespace WebHoster
 
         public WebBuilder UseWebRoot(string webRoot)
         {
-            _webRoot = webRoot;
+            this.webRoot = webRoot;
             return this;
         }
 
         public WebBuilder UseHttpPort(int port)
         {
-            _httpPort = port;
+            httpPort = port;
             return this;
         }
 
         public WebBuilder UseListenAddress(IPAddress address)
         {
-            _listenIP = address;
+            listenIP = address;
+            return this;
+        }
+
+        public WebBuilder UseCompression(bool use)
+        {
+            useCompression = use;
             return this;
         }
 
         public WebBuilder UseSSL(bool useSSL, int port = 443, string certificatePath = "", string certificatePassword = "")
         {
-            _useSSL = useSSL;
-            _sslPort = port;
-            _certificatePath = certificatePath;
-            _certificatePass = certificatePassword;
+            this.useSSL = useSSL;
+            sslPort = port;
+            sslCertificatePath = certificatePath;
+            sslCertificatePass = certificatePassword;
             return this;
         }
 
         public IWebHostBuilder Get()
         {
             // check if ssl is really usable
-            var sslUse = _useSSL && !string.IsNullOrEmpty(_certificatePath) && System.IO.File.Exists(_certificatePath);
+            var sslUse = useSSL && !string.IsNullOrEmpty(sslCertificatePath) && System.IO.File.Exists(sslCertificatePath);
 
-            var hb = WebHost.CreateDefaultBuilder();
+            var hostBuilder = WebHost.CreateDefaultBuilder();
 
             // set web root
-            if (!string.IsNullOrEmpty(_webRoot))
-                hb.UseWebRoot(_webRoot);
+            if (!string.IsNullOrEmpty(webRoot))
+                hostBuilder.UseWebRoot(webRoot);
 
             // user kestrel for multiplatform support & ssl
-            hb.UseKestrel(options =>
+            hostBuilder.UseKestrel(options =>
             {
                 options.AddServerHeader = false;
-                options.Listen(_listenIP, _httpPort);
+                options.Listen(listenIP, httpPort);
 
                 // if using ssl configure it
                 if (sslUse)
                 {
-                    options.Listen(_listenIP, _sslPort, config =>
+                    options.Listen(listenIP, sslPort, config =>
                     {
-                        if (string.IsNullOrEmpty(_certificatePass))
-                            config.UseHttps(_certificatePath);
+                        if (string.IsNullOrEmpty(sslCertificatePass))
+                            config.UseHttps(sslCertificatePath);
                         else
-                            config.UseHttps(_certificatePath, _certificatePass);
+                            config.UseHttps(sslCertificatePath, sslCertificatePass);
                     });
                 }
             });
 
             // pass injection info to startup
-            hb.ConfigureServices(sc => sc.AddSingleton<IStartupInjectionConfiguration>(_startupConfiguration));
+            hostBuilder.ConfigureServices(sc => sc.AddSingleton<IStartupInjectionConfiguration>(_startupConfiguration));
 
             // pass ssl info to startup too
-            hb.UseSetting("UseSSL", sslUse.ToString());
-            hb.UseSetting("RelaxedCors", relaxedCorsPolicy.ToString());
+            hostBuilder.UseSetting("UseSSL", sslUse.ToString());
+            hostBuilder.UseSetting("SSLport", sslPort.ToString());
+            hostBuilder.UseSetting("UseRelaxedCors", relaxedCorsPolicy.ToString());
+            hostBuilder.UseSetting("UseCompression", useCompression.ToString());
 
-            return hb.UseStartup<Startup>();
+            return hostBuilder.UseStartup<Startup>();
         }
 
     }
